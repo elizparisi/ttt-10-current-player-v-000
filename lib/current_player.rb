7bb360137c484d5980665c9efa037898f8ada9b9
Elizabeth Parisi <parisielizabeth@gmail.com>
2018-11-24 18:23:01 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,14 +2,14 @@ def turn_count(board)
   counter = 0
   board.each do |space|
     if space == "X" || space == "O"
-      counter +=1
+      counter += 1
   end
 end
 return counter
 end
 
 def current_player(board)
-  if turn_count(board) %2 == 0
+  if turn_count(board) % 2 == 0
     current_player = "X"
   else
     current_player = "O"
